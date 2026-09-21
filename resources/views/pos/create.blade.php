9b8b462e8fc23c6a553af56ada46a350f5acf3ad
cristanti-p <revalinakristantiputri@gmail.com>
2026-09-21 21:40:56 +0700

    tambah highlight kartu produk yang diklik

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -1,12 +1,9 @@
 @extends('layouts.app')
-
 @section('title', 'Kasir')
-
 @section('content')
 <h1 class="text-lg font-semibold mb-4">Transaksi Kasir</h1>
-
 <div x-data="{
-     cart: [],
+    cart: [],
     selectedId: null,
     addToCart(id, name, price) {
         this.cart.push({ id, name, price });
@@ -21,7 +18,7 @@
 }">
     <div class="grid grid-cols-3 gap-4">
         @foreach ($products as $product)
-            <div class="border rounded-md p-3 cursor-pointer"
+            <div class="border rounded-md p-3 cursor-pointer transition"
                  :class="selectedId === {{ $product->id }} ? 'ring-2 ring-blue-500' : ''"
                  @click="addToCart({{ $product->id }}, '{{ $product->name }}', {{ $product->price }})">
                 <p class="font-medium">{{ $product->name }}</p>
@@ -32,12 +29,14 @@
 
     <div class="mt-4 border-t pt-3">
         <template x-for="item in cart" :key="item.id">
-            <p x-text="item.name + ' - Rp ' + item.price"></p>
+            <div class="flex justify-between items-center">
+                <p x-text="item.name + ' - Rp ' + item.price"></p>
+                <button type="button"
+                        class="text-red-600 text-sm hover:underline"
+                        @click="removeFromCart(item.id)">Hapus</button>
+            </div>
         </template>
-
-        <p class="font-semibold mt-2">
-            Subtotal: Rp <span x-text="subtotal()"></span>
-        </p>
+        <p class="font-semibold mt-2">Subtotal: Rp <span x-text="subtotal()"></span></p>
     </div>
 </div>
 @endsection
