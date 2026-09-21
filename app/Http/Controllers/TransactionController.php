12dc5a080d7640a9ac2d09ba876781560835f638
cristanti-p <revalinakristantiputri@gmail.com>
2026-09-21 21:54:43 +0700

    tambah view riwayat transaksi

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -25,7 +25,7 @@ class TransactionController extends Controller
 
     public function index()
     {
-        return 'Daftar transaksi';
+        return view('transactions.index');
     }
 
     public function show(string $id)
