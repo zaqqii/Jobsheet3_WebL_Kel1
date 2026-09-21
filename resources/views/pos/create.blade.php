2e2d8519683dde1d78313100355c2e818701bf8b
cristanti-p <revalinakristantiputri@gmail.com>
2026-09-21 21:13:19 +0700

    tambah tombol hapus keranjang

--- resources/views/pos/create.blade.php
+++ resources/views/pos/create.blade.php
@@ -10,6 +10,9 @@
     addToCart(id, name, price) {
         this.cart.push({ id, name, price });
     },
+    removeFromCart(index) {
+        this.cart.splice(index, 1);
+    },
     subtotal() {
         return this.cart.reduce((sum, item) => sum + item.price, 0);
     }
@@ -30,12 +33,14 @@
 
     <div class="mt-4 border-t pt-3">
         <template x-for="item in cart" :key="item.id">
-            <p x-text="item.name + ' - Rp ' + item.price"></p>
+            <div class="flex justify-between items-center">
+                <p x-text="item.name + ' - Rp ' + item.price"></p>
+                <button type="button"
+                        class="text-red-600 text-sm hover:underline"
+                        @click="removeFromCart(item.id)">Hapus</button>
+            </div>
         </template>
-
-        <p class="font-semibold mt-2">
-            Subtotal: Rp <span x-text="subtotal()"></span>
-        </p>
+        <p class="font-semibold mt-2">Subtotal: Rp <span x-text="subtotal()"></span></p>
     </div>
 </div>
 @endsection
