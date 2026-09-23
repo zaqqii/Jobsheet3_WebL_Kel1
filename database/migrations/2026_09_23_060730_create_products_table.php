15cc3a020374069d63497fbe8046ac49120034ab
Nyza <syaqiqurrochman@gmail.com>
2026-09-23 15:12:10 +0700

    tambah migrasi categories dan products

--- database/migrations/2026_09_23_060730_create_products_table.php
+++ database/migrations/2026_09_23_060730_create_products_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained();
             $table->string('name');
+            $table->unsignedInteger('price');
+            $table->unsignedInteger('stock')->default(0);
             $table->timestamps();
         });
     }
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('products');
     }
 };
