bff02086ed8e78293e7816cca4051d2ad33d6d30
rifat djibran <rifatdjibran099@gmail.com>
2026-09-26 23:50:41 +0700

    pembetulan transaction migration

--- database/migrations/2026_09_23_063136_create_transactions_table.php
+++ database/migrations/2026_09_23_063136_create_transactions_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->unsignedInteger('total');
             $table->timestamps();
         });
     }
