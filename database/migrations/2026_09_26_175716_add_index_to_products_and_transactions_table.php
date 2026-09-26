bfca21efbfe73266ba0cc94cdd191be6adf81c13
rifat djibran <rifatdjibran099@gmail.com>
2026-09-27 01:04:27 +0700

    tambah seeder skala nyata dan index foreign key

---
+++ database/migrations/2026_09_26_175716_add_index_to_products_and_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->index('category_id');
+        });
+
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products_and_transactions', function (Blueprint $table) {
+            //
+        });
+    }
+};
